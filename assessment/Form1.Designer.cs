05933549bce56ed5bbe8c760110b7fb2a78c024c
jenson <jensonchew@gmail.com>
2015-10-16 14:52:39 +0800

    Simple Computation

--- assessment/Form1.Designer.cs
+++ assessment/Form1.Designer.cs
@@ -158,6 +158,7 @@
             this.button1.TabIndex = 13;
             this.button1.Text = "Calculate";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
--- assessment/Form1.cs
+++ assessment/Form1.cs
@@ -21,5 +21,46 @@ namespace assessment
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float price1;
+            float price2;
+            float discount;
+            float total;
+
+            if (textBox2.Text.Trim() == "")
+            {
+                price1 = 0;
+            }
+            else
+            {
+                price1 = float.Parse(this.textBox2.Text);
+            }
+
+            if (textBox4.Text.Trim() == "")
+            {
+                price2 = 0;
+            }
+            else
+            {
+                price2 = float.Parse(this.textBox4.Text);
+            }
+
+
+            if (textBox5.Text.Trim() == "")
+            {
+                discount = 0;
+            }
+            else
+            {
+                discount = float.Parse(this.textBox5.Text);
+            }
+
+
+            total = price1 + price2 - discount;
+
+            textBox6.Text = total.ToString();
+        }
     }
 }
